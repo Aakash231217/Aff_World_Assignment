3849531f98f18c6e38f87e4a8bd5e9e480201bdc
Aakash <rktaakash@gmail.com>
2024-01-17 20:30:06 +0530

    first commit

---
+++ build/precache-manifest.e55d16c72c77a401641c6574a0b12094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2beda01794b0f13920b93564fc4cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42fb5c8a60d6b8268945",
+    "url": "/static/css/main.fa508b23.chunk.css"
+  },
+  {
+    "revision": "1be584b2c430a92f6977",
+    "url": "/static/js/2.08da76db.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.08da76db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fb5c8a60d6b8268945",
+    "url": "/static/js/main.ccf8a193.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducers } from './reducers';
+import App from './App';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
